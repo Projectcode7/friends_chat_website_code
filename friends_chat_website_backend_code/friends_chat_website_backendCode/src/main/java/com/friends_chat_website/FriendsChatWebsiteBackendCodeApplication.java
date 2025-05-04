7c6da48a3713f169867fbcb9cb4822c3b50fca44
vijayrathore727 <vijayrathore727@gmail.com>
2025-05-04 13:50:41 +0530

    Project folder for backend

---
+++ friends_chat_website_backend_code/friends_chat_website_backendCode/src/main/java/com/friends_chat_website/FriendsChatWebsiteBackendCodeApplication.java
@@ -0,0 +1,13 @@
+package com.friends_chat_website;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendsChatWebsiteBackendCodeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FriendsChatWebsiteBackendCodeApplication.class, args);
+	}
+
+}
---
+++ friends_chat_website_backend_code/friends_chat_website_backendCode/src/test/java/com/friends_chat_website/FriendsChatWebsiteBackendCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.friends_chat_website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendsChatWebsiteBackendCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
